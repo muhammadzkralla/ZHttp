8aa7574e4d5bcee01408562c0f40f7a623634b82
Zkrallah <muhammad.heshamyt@gmail.com>
2024-06-19 17:09:06 +0300

    feat: Multipart request optimization to auto-serialize request body.

--- zhttp/src/main/java/com/zkrallah/zhttp/model/Basic.kt
+++ zhttp/src/main/java/com/zkrallah/zhttp/model/Basic.kt
@@ -1,5 +1,11 @@
 package com.zkrallah.zhttp.model
 
+/**
+ * Represents a basic token header.
+ *
+ * @property username The auth username.
+ * @property password The auth password.
+ */
 data class Basic(
     val username: String,
     val password: String
--- zhttp/src/main/java/com/zkrallah/zhttp/model/MultipartBody.kt
+++ zhttp/src/main/java/com/zkrallah/zhttp/model/MultipartBody.kt
@@ -5,14 +5,12 @@ package com.zkrallah.zhttp.model
  *
  * @property name The name of the multipart body part.
  * @property body The content body of the part, if applicable.
- * @property fileName The name of the file, if the part represents a file.
  * @property filePath The path to the file, if the part represents a file.
  * @property contentType The content type of the part, if specified.
  */
 data class MultipartBody(
     val name: String? = null,
-    val body: String? = null,
-    val fileName: String? = null,
+    val body: Any? = null,
     val filePath: String? = null,
     val contentType: String? = null
 )
